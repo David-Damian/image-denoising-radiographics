a0e7dc36a98bfb96cf4ec36b7f1fb9b743f04e6d
Brandon Hernández <brandonhdz2707@gmail.com>
2023-05-12 13:43:36 -0600

    store all images in two single folders

--- move_images_mura.py
+++ move_images_mura.py
@@ -1,19 +1,17 @@
 import os
 import shutil
 
-train_origin_path = os.path.join("MURA-v1.1", "train_image_paths.csv")
-valid_origin_path = os.path.join("MURA-v1.1", "valid_image_paths.csv")
+train_origin_path = os.path.join("data", "MURA-v1.1", "train_image_paths.csv")
+valid_origin_path = os.path.join("data", "MURA-v1.1", "valid_image_paths.csv")
 
-print(train_origin_path)
-
-train_dest_path = "DL_images/train"
-valid_dest_path = "DL_images/valid"
+train_dest_path = os.path.join("data", "DL_images", "train")
+valid_dest_path = os.path.join("data", "DL_images", "valid")
 
 with open(train_origin_path) as f:
     img_num = 1
     lines = f.readlines()
     for line in lines:
-        origin = line.strip()
+        origin = f'data/{line.strip()}'
         if os.path.exists(origin):
             shutil.move(origin, f'{train_dest_path}/image_{img_num}.png')
             img_num += 1
@@ -23,7 +21,7 @@ with open(valid_origin_path) as g:
     img_num = 1
     lines = g.readlines()
     for line in lines:
-        origin = line.strip()
+        origin = f'data/{line.strip()}'
         if os.path.exists(origin):
             shutil.move(origin, f'{valid_dest_path}/image_{img_num}.png')
             img_num += 1
